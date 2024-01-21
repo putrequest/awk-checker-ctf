df2edc46eaf41ffd6ea6fd137d5f2523f319e680
TypicalAM <tpam_piaseczny@protonmail.com>
2024-01-21 15:14:12 +0100

    Better app

--- src/app.py
+++ src/app.py
@@ -30,21 +30,24 @@ def home():
     if len(awk_command) > 5 and awk_command[0:5] == "2+2=5":
         raise ArithmeticError("2+2!=5")
 
+    print(f"Got request: {input_data}, {awk_command}")
     return awk_reseponse(run_query(input_data, awk_command))
 
 
 def run_query(input_data: str, awk_command: str) -> str:
     input_cmd = ["echo"] + input_data.split()
-    awk_cmd = ("awk", "{" + awk_command + "}")
     echo = subprocess.Popen(input_cmd, stdout=subprocess.PIPE)
+    # output = subprocess.check_output(
+    #     awk_cmd, stdin=echo.stdout).decode("utf-8")[:-1]
+    final_cmd = ('sudo', '-u', 'safe_awk', 'awk', "{"+awk_command+"}")
     output = subprocess.check_output(
-        awk_cmd, stdin=echo.stdout).decode("utf-8")[:-1]
+        final_cmd, stdin=echo.stdout).decode("utf-8")[:-1]
     echo.wait()
-    print(output)
+    print(f'Returning computed output: {output}')
     return output
 
 
-# logging.basicConfig(filename="important.log", level=logging.DEBUG)
+logging.basicConfig(filename="important.log", level=logging.DEBUG)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
